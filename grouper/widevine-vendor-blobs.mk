38d9cb489ce57a4a5b7b96544e6c28bc8c05fbe2
Harry Youd <harry@harryyoud.co.uk>
2017-02-17 16:46:59 +0000

    grouper: Last proprietary blobs, needed for DRM
    Change-Id: I85e36c2819c876d4ec9224ab430ab35643a4f14c

--- grouper/widevine-vendor-blobs.mk
+++ grouper/widevine-vendor-blobs.mk
@@ -6,4 +6,6 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libdrmmtkutil.so:system/vendor/lib/libdrmmtkutil.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/libQSEEComAPI.so:system/vendor/lib/libQSEEComAPI.so
